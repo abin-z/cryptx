bbe599f5ae74b1de9a607f37927c9e5498121352
abin <abin.z@outlook.com>
2025-11-10 00:28:20 +0800

    feat: 所有模式都必须提供 16 字节 IV

--- cryptx/src/aes.cc
+++ cryptx/src/aes.cc
@@ -11,10 +11,8 @@ cipher::cipher(const options& opts, bool encrypt) : opts_(opts), encrypt_(encryp
   // 检查 key 长度是否和 key_bits 匹配
   if (opts_.key.size() != static_cast<std::size_t>(opts_.key_bits))
     throw aes_exception("AES key length mismatch with key_bits");
-  if (opts_.iv.empty() && opts_.cipher_mode != mode::CTR && opts_.cipher_mode != mode::OFB &&
-      opts_.cipher_mode != mode::CFB)
-    throw aes_exception("IV must be provided for this AES mode");
-
+  // 所有模式都必须提供 16 字节 IV
+  if (opts_.iv.size() != 16) throw aes_exception("IV must be 16 bytes for AES encryption");
   init_context();
 }
 
